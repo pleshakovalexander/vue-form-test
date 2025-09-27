328cf77ecb38cec3f387b0e9a670c923ac93573d
Alexander Pleshakov <sh.plh@yandex.ru>
2025-09-27 17:59:30 +0300

    show record in app

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+const store = useRecordsStore()
+const add = () => store.addRecord()
+
+</script>
+
 <template>
   <UApp>
-    <NuxtWelcome />
+    <div class="flex gap-4">
+      <h1 class="text-2xl ">Учетные записи</h1>
+      <UButton @click="add()" icon="i-heroicons-plus-20-solid" color="primary" variant="solid" />
+    </div>
+
+    <div class="flex items-center gap-2 rounded-md bg-gray-50 p-2 text-sm text-gray-700">
+      <UIcon name="i-heroicons-question-mark-circle" class="h-5 w-5 text-gray-400 shrink-0" />
+      <p>
+        Для указания нескольких меток для одной пары логин/пароль используйте разделитель <code>;</code>
+      </p>
+    </div>
+
+    <div v-for="(record, index) in store.records" :key="index" class="flex gap-2 items-center">
+      <RecordItem :record="record" :index="index" />
+    </div>
+
   </UApp>
 </template>
